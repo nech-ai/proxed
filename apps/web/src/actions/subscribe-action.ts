0a559f0d3e5a17340c1147cd66faeff5b7f27777
Alex Vakhitov <alex.vakhitov@gmail.com>
2025-03-02 08:14:52 +0000

    feat: resend audience

--- apps/web/src/actions/subscribe-action.ts
+++ apps/web/src/actions/subscribe-action.ts
@@ -6,6 +6,11 @@ import { config } from "@config";
 export async function subscribeAction(formData: FormData) {
 	const email = formData.get("email") as string;
 
+	await resend.contacts.create({
+		email,
+		audienceId: process.env.RESEND_AUDIENCE_ID as string,
+	});
+
 	return resend.emails.create({
 		from: config.mailing.from,
 		to: ["alex@proxed.ai"],

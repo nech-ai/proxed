4f89668bcb0676a5aa15210106d4b06d260b3bf8
Alex Vakhitov <alex.vakhitov@gmail.com>
2026-01-02 12:16:59 +0700

    Re-export execution queries explicitly

--- packages/db/src/queries/index.ts
+++ packages/db/src/queries/index.ts
@@ -1,6 +1,10 @@
 export * from "./device-checks";
-export * from "./executions";
-export { updateExecution } from "./executions";
+export {
+	createExecution,
+	updateExecution,
+	type CreateExecutionParams,
+	type UpdateExecutionParams,
+} from "./executions";
 export * from "./projects";
 export * from "./server-keys";
 export * from "./teams";

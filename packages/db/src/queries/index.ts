27112044afe617d2a10763185f039ab45d319114
Alex Vakhitov <alex.vakhitov@gmail.com>
2026-01-02 12:11:34 +0700

    Export updateExecution

--- packages/db/src/queries/index.ts
+++ packages/db/src/queries/index.ts
@@ -1,5 +1,6 @@
 export * from "./device-checks";
 export * from "./executions";
+export { updateExecution } from "./executions";
 export * from "./projects";
 export * from "./server-keys";
 export * from "./teams";
